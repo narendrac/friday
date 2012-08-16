31b45f983bd35589b9a4a2c649318c7652d3f2b5
Narendra Chitrakar <narendrachitrakar@hotmail.com>
2012-08-16 19:22:38 +0545

    Added validation to form throught JSON just one done for minlenth

--- formcreator.php
+++ formcreator.php
@@ -2,26 +2,30 @@
     
     
 </form>
+
 <script type="application/javascript" src="js/jquery.js"></script>
 <script type="application/javascript" src="js/frmcrt.js"></script>
 <script>
     $(function(){
-        $(".myForm").frmcrt({
+        $("#myForm").frmcrt({
      inputs:{
          
             name:{
                 name:"name",
                 type:"text",
                 label:"Name",
-                cls:"longInput"
+                cls:"longInput",
+                rule:{
+                    'required':"true",
+                    'minlength':6
+                }
             },
-            check:{
+            checkboxs:{
                 name:"interest[]",
                 type:"checkbox",
                 label:"Play",
                 cls:"myCheck"
-            
-           },
+            },
            check1:{
                 name:"interest[]",
                 type:"checkbox",
--- js/frmcrt.js
+++ js/frmcrt.js
@@ -9,21 +9,46 @@
         
         var obj = $.extend(defaultVal, options);
         
-        console.log(obj);
-        var $thisform = this; 
+       // console.log(obj);
+        var $thisform = $(this);
+       var addErr = function($id, $msg){
+           $id.after("<label class='Err'>"+$msg+"</label>");
+       } 
+        //console.log(obj.inputs.length);
+            
+        var formElem = '';
         $.each(obj.inputs, function(key, val){
            switch (val.type){
                case "text":
-                     $("#myForm").append("<li><label>"+val.label+"</label><input type='text' class='"+val.cls+"' /></li>");  
+                   formElem +=  "<li><label>"+val.label+"</label><input type='text' class='"+val.cls+"' id='"+key+"'/></li>";  
                      break;
                case "checkbox":
-                   $("#myForm").append("<li><input type='checkbox' class='"+val.cls+"' /><label>"+val.label+"</label></li>");  
+                   formElem += "<li><input type='checkbox' class='"+val.cls+"' /><label>"+val.label+"</label></li>";  
                    break;
                case "password":
-                  $("#myForm").append("<li><label>"+val.label+"</label><input type='password' class='"+val.cls+"' /></li>");   
+                    formElem += "<li><label>"+val.label+"</label><input type='password' class='"+val.cls+"' /></li>";   
                   break;
                 }
-        })    
+        }) 
+       $thisform.append("<ul>"+formElem+"</ul>");
+      
+       $thisform.children().keyup(function(){
+           $(".Err").html("");
+           $.each(obj.inputs, function(ke, va){
+               
+                   $.each(va.rule, function(k, v){
+                       switch(k){
+                         case "minlength":
+                         if($("#"+ke).val().length < v){
+                             addErr($("#"+ke), "Enter "+v+" Characters");
+                         }        
+                         break;
+                       }
+                   });
+              
+           })
+       });
+       
        
     }
 })(jQuery);    
